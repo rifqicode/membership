a83615621bd7dd53ac5ea41b750eb174812189a0
rifqicode <muhammadrifqi.tb@gmail.com>
2017-10-17 12:55:49 +0700

    Add template AdminLTE

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,22 @@
-@extends('layouts.app')
+@extends('layouts.mainapp')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Dashboard
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li class="active">Dashboard</li>
+      </ol>
+    </section>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <!-- Main content -->
+    <section class="content">
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+    </section>
+    <!-- /.content -->
+  </div>
 @endsection
