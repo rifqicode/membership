8c2ee5fc8c1f8e24aca7165dc5d7e3f2a950720d
rifqicode <muhammadrifqi.tb@gmail.com>
2018-12-20 10:35:04 +0700

    heroku deploy

--- config/database.php
+++ config/database.php
@@ -54,18 +54,16 @@ return [
             'engine' => null,
         ],
 
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
+        'pgsql' => array(
+          'driver'   => 'pgsql',
+          'host'     => $host,
+          'database' => $database,
+          'username' => $username,
+          'password' => $password,
+          'charset'  => 'utf8',
+          'prefix'   => '',
+          'schema'   => 'public',
+        ),
 
         'sqlsrv' => [
             'driver' => 'sqlsrv',
